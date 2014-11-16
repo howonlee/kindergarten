db65fafdcc96e5264d550f4d53ed505345fc9f47
Howon Lee <howon@howonlee.com>
2014-11-16 00:38:52 -0800

    change the data

---
+++ www/js/data.js
@@ -0,0 +1,31 @@
+var MemoryStore = function(successCallback, errorCallback) {
+
+    this.findById = function(id, callback) {
+        var employees = this.employees;
+        var employee = null;
+        var l = employees.length;
+        for (var i=0; i < l; i++) {
+            if (employees[i].id === id) {
+                employee = employees[i];
+                break;
+            }
+        }
+        callLater(callback, employee);
+    }
+
+    // Used to simulate async calls. This is done to provide a consistent interface with stores (like WebSqlStore)
+    // that use async data access APIs
+    var callLater = function(callback, data) {
+        if (callback) {
+            setTimeout(function() {
+                callback(data);
+            });
+        }
+    }
+
+    this.classes = [];
+    this.office_hours = [];
+    this.quizzes = [];
+    this.polls = [];
+    callLater(successCallback);
+}
