e830ca5c794be037cdb5bcd902fa2f0fa1eceaba
Howon Lee <howon@howonlee.com>
2014-11-17 23:02:27 -0800

    compilation working?

--- www/js/ClassView.js
+++ www/js/ClassView.js
@@ -1,13 +1,13 @@
 var ClassView = function(store) {
   this.initialize = function() {
     this.el = $('<div/>');
-    this.el.on('keyup', '.search-key', this.findByName);
   }
   this.render = function() {
-    this.el.html(ClassView.template());
+    this.el.html(ClassView.homeTpl());
     return this;
   }
   this.initialize();
 }
 ClassView.homeTpl = Handlebars.compile($("#home-tpl").html());
 ClassView.employeeLiTpl = Handlebars.compile($("#class-li-tpl").html());
+ClassView.render();
