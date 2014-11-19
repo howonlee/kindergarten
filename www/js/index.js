9ea6a0aecf9de1e7c43a6fc491b1b53ef50d4f2e
Howon Lee <howon@howonlee.com>
2014-11-19 00:01:03 -0800

    still should be in a nonworking state

--- www/js/index.js
+++ www/js/index.js
@@ -3,21 +3,39 @@ var app = {
     var self = this;
     this.homeTpl = Handlebars.compile($("#home-tpl").html());
     this.classLiTpl = Handlebars.compile($("#class-li-tpl").html());
+    this.ohTpl = Handlebars.compile($("#oh-tpl").html());
+    this.ohUrl = /^#oh\/(\d{1,})/;
     this.adapter = new MemoryAdapter();
     this.adapter.initialize(function() {
-      var classView = new ClassView(self.adapter, self.homeTpl, self.classLiTpl);
-      $("body").html(classView.render().el);
-      classView.renderList();
+      this.route();
     });
     this.bindEvents();
   },
 
   bindEvents: function() {
     document.addEventListener('deviceready', this.onDeviceReady, false);
+    $(window).on("hashchange", this.route);
   },
   
   onDeviceReady: function() {
     app.receivedEvent('deviceready');
+  },
+
+  route: function() {
+    var hash = window.location.hash;
+    if (!hash) {
+      var classView = new ClassView(self.adapter, self.homeTpl, self.classLiTpl);
+      $("body").html(classView.render().el);
+      classView.renderList();
+      return;
+    }
+    var match = hash.match(this.ohUrl);
+    if (match) {
+      //implement findById now
+      adapter.findById(Number(match[1])).done(function(employee) {
+        $('body').html(new EmployeeView(adapter, employeeTpl, employee).render().el);
+      });
+    }
   },
 
   receivedEvent: function(id) {
